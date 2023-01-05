7f5e17dab95d21b830b95942262ffb4e7e2c1355
anjandash <anjan8@gmail.com>
2023-01-05 11:54:30 +0100

    added the Game model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import string
+import random
 
-# Create your models here.
+# generates an unique code for a 2-player game
+def generate_unique_code():
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=6)) 
+        if Game.objects.filter(code=code).count(0) == 0:
+            break 
+    return code
+
+# Creates the Game model
+class Game(models.Model):
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
