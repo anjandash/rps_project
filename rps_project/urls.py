d0df40d58170045a93fb02da1638aad89f0edbb9
anjandash <anjan8@gmail.com>
2023-01-05 14:19:05 +0100

    configured react frontend

--- rps_project/urls.py
+++ rps_project/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', include('frontend.urls'))
 ]
