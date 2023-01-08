4940679221ffc433ca7eb1577429d8e0d9350d40
anjandash <anjan8@gmail.com>
2023-01-08 01:47:58 +0100

    updated host_play_again and guest_play_again

--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,7 @@ from .models import Game
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game 
-        fields = ('id', 'code', 'host', 'guest', 'host_choice', 'guest_choice', 'created_at')
+        fields = ('id', 'code', 'host', 'guest', 'host_choice', 'host_play_again', 'guest_choice', 'guest_play_again', 'created_at')
 
 class JoinGameSerializer(serializers.ModelSerializer):
     class Meta:
